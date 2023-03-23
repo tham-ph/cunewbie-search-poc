d8ef3ec08904e270cd93a96a859502975495fe64
Thammarit Phiwton <poomz180@gmail.com>
2023-03-23 19:56:02 +0700

    feat: mysql -> logstash -> rabbitmq -> logstash -> elasticsearch

--- backend-service/src/main.go
+++ backend-service/src/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"context"
-	amqp "github.com/rabbitmq/amqp091-go"
 	"github.com/tham-ph/cunewbie-search-poc/backend-service/src/database"
 	"gorm.io/gorm"
 	"log"
-	"time"
 )
 
 type Book struct {
@@ -37,52 +34,52 @@ func main() {
 		log.Fatal(err)
 	}
 
-	//db.Create(&Book{Title: "P123", Author: "J.R.R. Tolkien", Rating: 4, Voters: 100, Price: 10.99, Currency: "USD", Description: "A great book", Publisher: "Allen & Unwin", PageCount: 295, Genres: "Fantasy", ISBN: "9780241956400", Language: "English", PublishedDate: "21 September 1937"})
+	db.Create(&Book{Title: "5x", Author: "J.R.R. Tolkien", Rating: 4, Voters: 100, Price: 10.99, Currency: "USD", Description: "A great book", Publisher: "Allen & Unwin", PageCount: 295, Genres: "Fantasy", ISBN: "9780241956400", Language: "English", PublishedDate: "21 September 1937"})
 
 	//res := Book{}
 	//db.Model(&Book{}).Where("title = ?", "Poom Book").First(&res)
 	//res.Title = "Poom Book 99"
 	//db.Save(&res)
 
-	db.Delete(&Book{}, 38)
+	//db.Delete(&Book{}, 46)
 
-	rabbitmqConnection, err := database.ConnectRabbitMQ()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rabbitmqConnection.Close()
-
-	rabbitmqChannel, err := rabbitmqConnection.Channel()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rabbitmqChannel.Close()
-
-	err = rabbitmqChannel.ExchangeDeclare(
-		"exchange1",
-		"direct",
-		true,
-		false,
-		false,
-		false,
-		nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	err = rabbitmqChannel.PublishWithContext(ctx,
-		"exchange1",
-		"sync_mysql_es",
-		false,
-		false,
-		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte("Hello World! with direct exchange"),
-		})
-	if err != nil {
-		log.Fatal(err)
-	}
+	//rabbitmqConnection, err := database.ConnectRabbitMQ()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//defer rabbitmqConnection.Close()
+	//
+	//rabbitmqChannel, err := rabbitmqConnection.Channel()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//defer rabbitmqChannel.Close()
+	//
+	//err = rabbitmqChannel.ExchangeDeclare(
+	//	"exchange1",
+	//	"direct",
+	//	true,
+	//	false,
+	//	false,
+	//	false,
+	//	nil)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	//defer cancel()
+	//err = rabbitmqChannel.PublishWithContext(ctx,
+	//	"exchange1",
+	//	"sync_mysql_es",
+	//	false,
+	//	false,
+	//	amqp.Publishing{
+	//		ContentType: "text/plain",
+	//		Body:        []byte("Hello World! with direct exchange"),
+	//	})
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 
 }
